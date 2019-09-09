74da2571aeaee5d34cdff118ea43d577d2104044
nagyrobert1996 <43435316+nagyrobert1996@users.noreply.github.com>
2019-09-09 15:00:14 +0200

    Add files via upload

---
+++ Yoda.java
@@ -0,0 +1,9 @@
+public class Yoda
+{
+public static void main (String[]args)
+{
+String myString = null;
+if (myString.equals("valami")) {  System.out.println("+");}
+//if ("valami".equals(myString)) {  System.out.println("++");}
+}
+}
---
+++ gagyi.java
@@ -0,0 +1,11 @@
+public class gagyi
+{
+public static void main (String[]args)
+{
+Integer x = -129;
+Integer t = -129;
+System.out.println (x);
+System.out.println (t);
+while (x <= t && x >= t && t != x);
+}
+}
